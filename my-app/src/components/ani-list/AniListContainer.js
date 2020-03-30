7fa2c111a68d3a27244750eb83fe86b11f54ba76
mar-jac <mar.jacq0296@gmail.com>
2020-03-30 12:36:11 -0400

    Added more anime and user containers.

---
+++ my-app/src/components/ani-list/AniListContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react"
+
+export default class AniListContainer extends Component {
+    render() {
+        return (
+            <div>       
+           This is a anime list page.
+            </div>
+        )
+    }
+}
---
+++ my-app/src/components/user-list/UserListContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react"
+
+export default class UserListContainer extends Component {
+    render() {
+        return (
+            <div>       
+           This is a user list page.
+            </div>
+        )
+    }
+}
