8c4e267d52b420fde44414ba4151c72001af1ca7
mar-jac <mar.jacq0296@gmail.com>
2020-03-30 12:32:46 -0400

    Imported toastr to index and added more pages.

--- my-app/src/actions/index.js
+++ my-app/src/actions/index.js
@@ -1,9 +1,38 @@
 import alt from "../alt"
+import toastr from "toastr"
 
 class Actions {
-    getAnime(payload) {
+    getAllAnime(payload) {
         return payload
     }
+
+    deleteAnime(id) {
+    toastr.success(`Deleted anime with this id ${id} from Miru table`)
+    return id
+    }
+
+    resetAnimeTable(payload) {
+        toastr.success("Reset the table full of anime")
+        return payload
+    }
+
+    resetAnimeGenreTable(payload) {
+        toastr.success("Reset genres.")
+        return payload
+    }
+    resetUserTable(payload) {
+        toastr.success("Reset the user table.")
+        return payload
+    }
+    resetReviewTable(payload) {
+        toastr.success("Reset the reviews.")
+        return payload
+    }
+    resetUserReviewTable(payload) {
+        toastr.success("Reset the user review table.")
+        return payload
+    }
+
 }
 
 export default alt.createActions(Actions)
