36c8551ae769fb042eee72733e63f4e35515e361
mar-jac <mar.jacq0296@gmail.com>
2020-04-14 13:23:45 -0400

    Finalizing setup and last files.

--- my-app/src/actions/index.js
+++ my-app/src/actions/index.js
@@ -1,38 +1,95 @@
-import alt from "../alt"
-import toastr from "toastr"
+import alt from '../alt'
+import history from '../history'
+import toastr from 'toastr'
 
 class Actions {
-    getAllAnime(payload) {
-        return payload
-    }
-
-    deleteAnime(id) {
-    toastr.success(`Deleted anime with id ${id} from Miru table`)
+  getAnime(id) {
     return id
-    }
+  }
 
-    resetAnimeTable(payload) {
-        toastr.success("Reset the table full of anime")
-        return payload
-    }
+  getAnimes(payload) {
+    return payload
+  }
 
-    resetAnimeGenreTable(payload) {
-        toastr.success("Reset genres.")
-        return payload
-    }
-    resetUserTable(payload) {
-        toastr.success("Reset the user table.")
-        return payload
-    }
-    resetReviewTable(payload) {
-        toastr.success("Reset the reviews.")
-        return payload
-    }
-    resetUserReviewTable(payload) {
-        toastr.success("Reset the user review table.")
-        return payload
-    }
+  deleteAnime(id) {
+    toastr.success(`Deleted anime with id ${id} from Anime table!`)
+    return id
+  }
 
+  resetAnimeTable(payload) {
+    toastr.success(`Reset Anime table!`)
+    return payload
+  }
+  
+  getReviews(id) {
+    return id
+  }
+
+  getGenres(id) {
+    return id
+  }
+
+  addAnime(anime) {
+    const { name, type } = anime
+    toastr.success(`Added the ${type.toLowerCase()} ${name} to the Anime table!`)
+    history.push('/home')
+    return anime
+  }
+
+  getAllGenres(payload) {
+    return payload
+  }
+
+  getLargestAnimeId(payload) {
+    return payload
+  }
+
+  resetAnimeGenreTable(payload) {
+    toastr.success(`Reset Anime_Genre table!`)
+    return payload
+  }
+
+  getAllUsers(garbage) {
+    return garbage
+  }
+
+  getUser(id) {
+    return id
+  }
+
+  editUser(user) {
+    const {user_id, username } = user 
+    toastr.success(`Updated ${username} with user id ${user_id}!`)
+    history.push(`/user`)
+    return user
+  }
+  
+  resetUserTable(payload) {
+    toastr.success(`Reset Users table!`)
+    return payload
+  }
+
+  loginUser(payload) {
+    return payload
+  }
+
+  getLoggedInUser(garbage) {
+    return garbage
+  }
+
+  postReview(review) {
+    return review
+  }
+
+  resetReviewTable(payload) {
+    toastr.success('Reset Reviews table!')
+    return payload
+  }
+
+  resetUserReviewTable(payload) {
+    toastr.success('Reset User_Review table!')
+    return payload
+  }
 }
 
 export default alt.createActions(Actions)
--- my-app/src/stores/AnimeStore.js
+++ my-app/src/stores/AnimeStore.js
@@ -1,52 +1,136 @@
-import alt from "../alt"
-import Actions from "../actions"
+import alt from '../alt'
+import Actions from '../actions'
 
-const allURL = "http://localhost:5000/allAnime/"
-const deleteURL = "http://localhost:5000/deleteAnime/"
-const resetURL = "http://localhost:5000/resetAnime"
-const resetAnimeGenreURL = "http://localhost:5000/resetAnimeGenre"
+const axios = require('axios')
+
+const allUrl = 'http://localhost:4000/allAnime'
+const singleUrl = 'http://localhost:4000/animeById/'
+const deleteUrl = 'http://localhost:4000/deleteAnime/'
+const resetUrl = 'http://localhost:4000/resetAnime'
+const reviewsUrl = 'http://localhost:4000/reviewsByAnimeId/'
+const genresUrl = 'http://localhost:4000/genresByAnimeId/'
+const addUrl = 'http://localhost:4000/addAnime/'
+const resetAnimeGenreUrl = 'http://localhost:4000/resetAnimeGenre'
+const postReviewUrl = 'http://localhost:4000/postReview/'
+const resetReviewUrl = 'http://localhost:4000/resetReviews'
 
 class AnimeStore {
-    constructor() {
-        this.allAnime = []
+  constructor() {
+    this.animes = []
+    this.anime = {}
+    this.reviews = []
+    this.genres = []
 
-        this.bindListeners({
-            handleAnime: Actions.GET_ALL_ANIME,
-            handleDeleteAnime: Actions.DELETE_ANIME,
-            handleResetAnime: Actions.RESET_ANIME_TABLE,
-            handleResetAnimeGenre: Actions.RESET_ANIME_GENRE_TABLE
-        })
-    }
+    this.bindListeners({
+      handleGetAnime: Actions.GET_ANIME,
+      handleGetAnimes: Actions.GET_ANIMES,
+      handleDeleteAnime: Actions.DELETE_ANIME,
+      handleResetAnime: Actions.RESET_ANIME_TABLE,
+      handleGetReviews: Actions.GET_REVIEWS,
+      handleGetGenres: Actions.GET_GENRES,
+      handleAddAnime: Actions.ADD_ANIME,
+      handleResetAnimeGenre: Actions.RESET_ANIME_GENRE_TABLE,
+      handlePostReview: Actions.POST_REVIEW,
+      handleResetReview: Actions.RESET_REVIEW_TABLE
+    })
+  }
 
-    handleAnime = payload => {
-        console.log("Get anime from AnimeStore.");
+  handleGetAnime = id => {
+    console.log(`AnimeStore :: handle get anime on anime_id ${id}`)
+    fetch(`${singleUrl}${id}`)
+      .then(res => res.json())
+      .then(json => {
+        return this.setState({ anime: json[0] })
+      })
+  }
 
-        fetch(allURL)
-            .then(res => res.json())
-            .then(json => {
-                return this.setState({ allAnime: json })
-            })
-    }
+  handleGetAnimes = garbage => {
+    console.log(`AnimeStore :: handle get animes w/ ${garbage}`)
+    fetch(allUrl)
+      .then(res => res.json())
+      .then(json => {
+        return this.setState({ animes: json })
+    })
+  }
 
-    handleDeleteAnime = id => {
-        console.log(`AnimeStore :: handle delete called on uid ${id}`)
-        fetch(`${deleteURL}${id}`).then(() => {
-          this.handleAnime("")
-        })
-      }
+  handleDeleteAnime = id => {
+    console.log(`AnimeStore :: handle delete called on anime_id ${id}`)
+    fetch(`${deleteUrl}${id}`).then(() => {
+      this.handleGetAnimes("")
+    })
+  }
 
-    handleResetAnime = payload => {
-        console.log(`AnimeStore :: handle reset anime table ${payload}`)
-        fetch(resetURL).then(() => {
-            this.handleAnime("")
-        })
-    }
+  handleResetAnime = garbage => {
+    console.log(`AnimeStore :: handle reset anime table w/ ${garbage}`)
+    fetch(resetUrl).then(() => {
+      this.handleGetAnimes("")
+    })
+  }
 
-    handleResetAnimeGenre = payload => {
-        console.log(`AnimeStore :: handle reset anime genre table that has a ${payload}`)
-        fetch(`${resetAnimeGenreURL}`)
-    }
+  handleGetReviews = id => {
+    console.log(`AnimeStore :: handle get reviews w/ anime_id ${id}`)
+    fetch(`${reviewsUrl}${id}`)
+      .then(res => res.json())
+      .then(json => {
+        return this.setState({ reviews: json })
+      })
+  }
+
+  handleGetGenres = id => {
+    console.log(`AnimeStore :: handle get genres w/ anime_id ${id}`)
+    fetch(`${genresUrl}${id}`)
+      .then(res => res.json())
+      .then(json => {
+        return this.setState({ genres: json })
+      })
+  }
+
+  handleAddAnime = anime => {
+    console.log(`AnimeStore :: handle add anime w/ anime ${anime}`)
+
+    // FIX!!! genres: anime.genre
+    axios.post(`${addUrl}:name/:animeId/:type/:episodes/:rating/:members/:genres`, {
+      name: anime.name,
+      animeId: anime.animeId,
+      type: anime.type,
+      episodes: anime.episodes,
+      rating: anime.rating,
+      members: anime.members,
+      genres: anime.genres
+    }).then(() => {
+      this.handleGetAnimes("")
+    }).catch(err => {
+      console.log('handleAddAnime error!')
+      console.log(err)
+    })
+  }
+
+  handleResetAnimeGenre = garbage => {
+    console.log(`AnimeStore :: handle reset anime_genres table w/ ${garbage}`)
+    fetch(`${resetAnimeGenreUrl}`)
+  }
+
+  handlePostReview = reviewInfo => {
+    console.log(`UserStore :: handle post review w/ review info ${JSON.stringify(reviewInfo)}`)
+    const { review, anime_id, user_id, date_time } = reviewInfo
+    axios.post(`${postReviewUrl}:review/:animeId/:userId/:dateTime`, {
+      review,
+      animeId: anime_id,
+      userId: user_id,
+      dateTime: date_time
+    }).then(() => {
+      this.handleGetReviews(anime_id)
+    }).catch(err => {
+      console.log('handlePostReview error!')
+      console.log(err)
+    })
+  }
+
+  handleResetReview = garbage => {
+    console.log(`AnimeStore :: handle reset reviews table w/ ${garbage}`)
+    fetch(resetReviewUrl)
+  }
 }
 
-export default AnimeStore = alt.createStore(AnimeStore, "AnimeStore")
+export default AnimeStore = alt.createStore(AnimeStore, 'AnimeStore')
 window.AnimeStore = AnimeStore;
